3544919db5244fbef0d9d3bdfb1654473e5bc22a
Cedrik Nikita <nikita.cedrik@gmail.com>
2022-10-14 11:10:49 +0400

    style(modal): remove unused selector

--- src/popup/components/Modal.vue
+++ src/popup/components/Modal.vue
@@ -146,18 +146,6 @@ export default {
       width: calc(#{variables.$extension-width} - 32px);
     }
 
-    .close-button {
-      position: absolute;
-      z-index: 3;
-      right: 8px;
-      top: 8px;
-      color: variables.$color-white;
-
-      &-icon {
-        width: 100%;
-      }
-    }
-
     .header {
       flex-basis: 32px;
       flex-shrink: 0;
